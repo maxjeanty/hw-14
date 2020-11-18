dc4b280f3d874c7c9deefadd59685becb00e3708
maxjeanty <67810350+maxjeanty@users.noreply.github.com>
2020-11-17 20:19:59 -0600

    Add files via upload
    made sure files were out of develop folder

---
+++ server.js
@@ -0,0 +1,33 @@
+// Requiring necessary npm packages  
+// this creates instances of the express and passport packages to run the application this is the main page that runs the application
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
+// Setting up port and requiring models for syncing
+// the application listens to this port to get all the data flowing
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+// db  is the database that contains all info to be displayed
+
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
